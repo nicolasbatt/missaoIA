e74371afbb0a7e3cb13ff24ad96c7e0856c80fa2
nicolasbatt <134967117+nicolasbatt@users.noreply.github.com>
2025-09-09 14:36:44 -0300

    aula9

--- srclpt.js
+++ srclpt.js
@@ -74,7 +74,9 @@ function mostraPergunta() {
     }
 
     function mostraResultado() {
-     caixaPerguntas.textContent = "Em 2049...";   
+     caixaPerguntas.textContent = "Em 2049...";  
+     textoResultado.textContent=historiaFinal;
+     caixaAlternativas.textContent="" ;
 
 
 
